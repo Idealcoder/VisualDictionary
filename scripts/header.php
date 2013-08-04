e1316ebe5af2dbb3c8cab80b8e19790b86487665
Idealcoder <idealcoder@idealcoder.co.uk>
2013-08-04 18:13:08 +0100

    Another test

--- scripts/header.php
+++ scripts/header.php
@@ -6,7 +6,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title><?php echo $pagetitle; ?> | YRS-2013</title>
+        <title><?php echo $pagetitle; ?> | YRS-2013 Test</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width">
 
