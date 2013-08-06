468e18dda454b3a89cf4246506c68bd0fb727727
Idealcoder <idealcoder@idealcoder.co.uk>
2013-08-06 18:54:31 +0100

    Made basic prototype of interface connecting together backend first time

--- index.php
+++ index.php
@@ -25,19 +25,55 @@ echo '			<a href="/'.$row["accro"].'/index2.php"><img src="/static/img/flags/'.$
 		
 		
 		<section style="width:500px;">
+			<br>
 			
-			<?php echo $messages[2]; ?><br>
+			<div style="text-align:center;width:40%;float:left;">
+			<?php
+			$stmt=$dbh->prepare("SELECT * FROM `languages`");
+			$stmt->execute();
+
+			while ($row = $stmt->fetch()) {			
+echo '			<a href="/'.$row["accro"].'/index2.php"><img src="/static/img/flags/'.$row["flagurl"].'" /></a>';
+echo '			<a href="/'.$row["accro"].'/index2.php"><img src="/static/img/flags/'.$row["flagurl"].'" /></a>';
+echo '			<a href="/'.$row["accro"].'/index2.php"><img src="/static/img/flags/'.$row["flagurl"].'" /></a>';
+			}
 			
-			<img width="80%" src="/static/img/tags/Red_Apple.jpg" />
-			<br><br>
-			<form method="POST" action="addtag.php">
+			?>
+			</div>
+			
+			<div style="text-align:center;width:20%;float:left;">
+			<button><i class="icon-exchange icon-2x"></i></button>
+			</div>
+			
+			<div style="text-align:center;width:40%;float:left;">
+			<?php
+			$stmt=$dbh->prepare("SELECT * FROM `languages`");
+			$stmt->execute();
+
+			while ($row = $stmt->fetch()) {			
+echo '			<a href="/'.$row["accro"].'/index2.php"><img src="/static/img/flags/'.$row["flagurl"].'" /></a>';
+echo '			<a href="/'.$row["accro"].'/index2.php"><img src="/static/img/flags/'.$row["flagurl"].'" /></a>';
+echo '			<a href="/'.$row["accro"].'/index2.php"><img src="/static/img/flags/'.$row["flagurl"].'" /></a>';
+			}
+			
+			?>
+			</div>
+			
+			<input style="padding:" type="text" placeholder="Type Translation Here" /><br><br>
+			
+			<img width="40%" height="200px" src="/static/img/tags/Red_Apple.jpg" />
+			<img width="40%" height="200px" src="/static/img/tags/Apples.jpg" />
+			<br>
+			Pomme
+			
+			<!-- <form method="POST" action="addtag.php">
 				<input type="hidden" name="imageid" value="3">
 				<input type="radio" name="toogeneric" value="0" checked="checked"><textarea type="text" name="name"></textarea><br>
 				<input type="radio" name="toogeneric" value="1">Too Generic<br>
 				
 				
 				<div style="text-align:right;margin:0.3em;"><button href="/en/index.php">Differen't Image</button><button type="submit">Ok</button></div>
-			</form>
+			</form> -->
 			
 		</section>
 		<br><br>
---
+++ index2.php
@@ -0,0 +1,48 @@
+﻿<?php 
+session_start();
+$root = realpath($_SERVER["DOCUMENT_ROOT"]);
+$pagetitle="Add Tag";
+include("$root/scripts/db-connection.php"); 
+include("$root/scripts/header.php"); 
+include("$root/scripts/settings.php");
+?>
+		
+		<div id ="settings">
+			<?php
+			$stmt=$dbh->prepare("SELECT * FROM `languages`");
+			$stmt->execute();
+
+			while ($row = $stmt->fetch()) {			
+echo '			<a href="/'.$row["accro"].'/index2.php"><img src="/static/img/flags/'.$row["flagurl"].'" /></a>';
+			}
+			
+			?>
+		
+		</div>
+		<nav>
+			<a href=""><i id="settingsbutton" class="icon-cogs icon-2x"></i></a>
+		</nav>
+		<br>
+		
+		
+		<section style="width:500px;">
+			
+			<?php echo $messages[2]; ?><br>
+			
+			<img width="80%" src="/static/img/tags/Red_Apple.jpg" />
+			<br><br>
+			<form method="POST" action="addtag.php">
+				<input type="hidden" name="imageid" value="3">
+				<input type="radio" name="toogeneric" value="0" checked="checked"><textarea type="text" name="name"></textarea><br>
+				<input type="radio" name="toogeneric" value="1">Too Generic<br>
+				
+				
+				<div style="text-align:right;margin:0.3em;"><button href="/en/index.php">Differen't Image</button><button type="submit">Ok</button></div>
+			</form>
+			
+		</section>
+		<br><br>
+		
+<?php 
+include("$root/scripts/footer.php"); 
+?>
