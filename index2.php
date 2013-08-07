2f74ff924cf680b89695e015a75dbac5e28f06b1
Idealcoder <idealcoder@idealcoder.co.uk>
2013-08-07 13:28:35 +0100

    Removed Navbar. Added some padding to compliment styling.

--- index2.php
+++ index2.php
@@ -1,47 +1,29 @@
-﻿<?php 
+<?php 
 session_start();
 $root = realpath($_SERVER["DOCUMENT_ROOT"]);
 $pagetitle="Add Tag";
 include("$root/scripts/db-connection.php"); 
-include("$root/scripts/header.php"); 
 include("$root/scripts/settings.php");
-?>
-		
-		<div id ="settings">
-			<?php
-			$stmt=$dbh->prepare("SELECT * FROM `languages`");
-			$stmt->execute();
+include("$root/scripts/header.php"); 
 
-			while ($row = $stmt->fetch()) {			
-echo '			<a href="/'.$row["accro"].'/index2.php"><img src="/static/img/flags/'.$row["flagurl"].'" /></a>';
-			}
-			
-			?>
+?>
+		<section style="max-width:500px;">
 		
-		</div>
-		<nav>
-			<a href=""><i id="settingsbutton" class="icon-cogs icon-2x"></i></a>
-		</nav>
-		<br>
-		
-		
-		<section style="width:500px;">
 			
-			<?php echo $messages[2]; ?><br>
+			<?php echo $messages[2]; ?>:<div style="margin:0.5em"></div>
 			
-			<img width="80%" src="/static/img/tags/Red_Apple.jpg" />
-			<br><br>
+			<div class="tag-image-wrapper" style="text-align:center"><img class="tag-image" src="/static/img/tags/Red_Apple.jpg" /></div>
 			<form method="POST" action="addtag.php">
 				<input type="hidden" name="imageid" value="3">
 				<input type="radio" name="toogeneric" value="0" checked="checked"><textarea type="text" name="name"></textarea><br>
-				<input type="radio" name="toogeneric" value="1">Too Generic<br>
+				<div style="margin:0.5em"></div>
+				<input type="radio" name="toogeneric" value="1"> <?php echo $messages[3]; ?><br>
 				
 				
-				<div style="text-align:right;margin:0.3em;"><button href="/en/index.php">Differen't Image</button><button type="submit">Ok</button></div>
+				<div style="text-align:right;margin:0.3em;"><a style="padding-bottom:5px" class="button" href="/en/index.php"><?php echo $messages[4]; ?></a><button class="button" type="submit">Ok</button></div>
 			</form>
 			
 		</section>
-		<br><br>
 		
 <?php 
 include("$root/scripts/footer.php"); 
