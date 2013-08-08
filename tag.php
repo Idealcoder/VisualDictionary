14b9b793a137233d3ee6ec50af41c735c2a9984b
Idealcoder <idealcoder@idealcoder.co.uk>
2013-08-08 20:48:51 +0100

    Fixing html error from previous commit

--- tag.php
+++ tag.php
@@ -11,7 +11,7 @@ include("$root/scripts/header.php");
 			
 			<?php echo $messages[2]; ?>:<div style="margin:0.5em"></div>
 			
-			<div class="tag-image-wrapper" style="text-align:center"><img height="320px" width="320px" id="inputimage" class="tag-image" src="" /></div>
+			<div class="tag-image-wrapper" style="text-align:center"><img height="320" width="320" id="inputimage" class="tag-image" src="" /></div>
 			<form method="GET" action="api.php">
 				<input type="hidden" name="type" value="addtag">
 				<input id="hiddeninput" type="hidden" name="imageid" value="3">
